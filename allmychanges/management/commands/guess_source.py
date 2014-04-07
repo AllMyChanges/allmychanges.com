a24e69b94a0a7326b6529d54cbb5da135acbb9d6
Alexander Artemenko <svetlyak.40wt@gmail.com>
2014-04-07 12:42:57 +0400

    Added a command to test source guesser.

---
+++ allmychanges/management/commands/guess_source.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+from django.core.management.base import BaseCommand
+
+from allmychanges.utils import guess_source
+from allmychanges.models import Package, Version
+
+
+class Command(BaseCommand):
+    help = u"""Command to test VCS log extractors' second step — version extraction."""
+
+    def handle(self, *args, **options):
+        if args:
+            packages = [('python', name, None)
+                        for name in args]
+            show_match = False
+        else:
+            packages = [(p.namespace, p.name, p)
+                        for p in Package.objects.all()]
+            show_match = True
+
+        total = len(packages)
+        matched = 0
+        have_guesses = 0
+        
+        for namespace, name, package in packages:
+            urls = list(guess_source(namespace, name))
+            if urls:
+                have_guesses += 1
+                
+                if show_match:
+                    if package.source in urls:
+                        matched += 1
+                    else:
+                        print ''
+                        print '!!! Handwritten source is: ', package.source
+                
+            print '{0}/{1}: {2}'.format(
+                namespace, name, urls)
+
+        if show_match:
+            print 'For {0:.4}% packages, hand choosen url was also among guessed.'.format(
+                float(matched) / total * 100)
+            
+        print 'For {0:.4}% packages, we have some guesses.'.format(
+                float(have_guesses) / total * 100)
+
+
