2edef19d85ade3ac6f519c07c74266bdc4cf9ec8
Alexander Artemenko <svetlyak.40wt@gmail.com>
2014-04-11 23:01:31 +0400

    Nicer button's color.

--- allmychanges/settings/auth.py
+++ allmychanges/settings/auth.py
@@ -4,8 +4,11 @@ AUTHENTICATION_BACKENDS = (
 )
 
 
+# https://apps.twitter.com/
 SOCIAL_AUTH_TWITTER_KEY        = 'zfUBhL9cF0B44r33AzszA'
 SOCIAL_AUTH_TWITTER_SECRET     = 'v0rfjbdVUozKWjVSxQ0Wh5h0RKuZQy9CTIu35L9KLI'
+
+# https://github.com/settings/applications
 SOCIAL_AUTH_GITHUB_KEY               = '7349d3551b5d86c85be1'
 SOCIAL_AUTH_GITHUB_SECRET           = '6606dc0b084d8e304c0fbd5c794f4901c2785198'
 
