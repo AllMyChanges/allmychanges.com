bbc830493e146fe8f11f2b38bf06ba86f1a94e68
Alexander Artemenko <svetlyak.40wt@gmail.com>
2014-05-01 20:03:32 +0000

    Added new command check_version_consistency.

---
+++ allmychanges/management/commands/check_version_consistency.py
@@ -0,0 +1,45 @@
+from django.core.management.base import BaseCommand
+from twiggy_goodies.django import LogMixin
+from allmychanges.models import Package
+
+
+def parse_version(text):
+    try:
+        return tuple(map(int, text.split('.')))
+    except:
+        return ()
+
+
+def check_versions(versions):
+    if versions:
+        previous = versions[0]
+        for version in versions[1:]:
+            if version < previous:
+                return (previous, version)
+            previous = version
+    return None
+
+
+def check_package(package):
+    versions = package.changelog.versions.all().order_by('id')
+    numbers = [parse_version(v.number) for v  in versions]
+    return check_versions(numbers)
+
+
+class Command(LogMixin, BaseCommand):
+    help = u"""Tests crawler on selected projects."""
+
+    def handle(self, *args, **options):
+        packages = Package.objects.all()
+        num_problems = 0
+        
+        for package in packages:
+            result = check_package(package)
+            if result:
+                num_problems += 1
+                prev_version, version = result
+                print package.namespace, package.name, '{0} > {1}'.format(
+                    '.'.join(map(str, prev_version)),
+                    '.'.join(map(str, version)))
+
+        print 'Num problems:', num_problems
