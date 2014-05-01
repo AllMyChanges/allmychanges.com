c239d3d15901953be19664107c31392717a06e78
Alexander Artemenko <svetlyak.40wt@gmail.com>
2014-05-01 20:03:16 +0000

    Command update_package was fixed.

--- allmychanges/management/commands/update_package.py
+++ allmychanges/management/commands/update_package.py
@@ -1,12 +1,16 @@
 from django.core.management.base import BaseCommand
 from twiggy_goodies.django import LogMixin
 from allmychanges.models import Package
-from allmychanges.tasks import update_package
+from allmychanges.tasks import update_changelog_task
 
 class Command(LogMixin, BaseCommand):
     help = u"""Tests crawler on selected projects."""
 
     def handle(self, *args, **options):
         name = args[0]
-        package = Package.objects.get(name=name)
-        update_package(package.id)
+        package = Package.objects.filter(name=name)[0]
+        
+        if len(args) > 1 and args[1] == 'full':
+            package.changelog.versions.all().delete()
+
+        update_changelog_task(package.changelog.source)
