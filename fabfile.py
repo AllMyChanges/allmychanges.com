470030f8cd4e4dc90d3390c3f2d3e78cfba7c255
Alexander Artemenko <svetlyak.40wt@gmail.com>
2014-06-18 11:27:17 +0000

    Now all requirements have sources.

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,7 @@
 from fabric.api import local
 
 def update_requirements():
-    local('pip-compile requirements.in')
-    local('pip-compile requirements-dev.in')
-#    local('pip-compile --include-sources requirements.in')
-#    local('pip-compile --include-sources requirements-dev.in')
+    local('pip-compile --include-sources requirements.in')
+    local('pip-compile --include-sources requirements-dev.in')
 
     
