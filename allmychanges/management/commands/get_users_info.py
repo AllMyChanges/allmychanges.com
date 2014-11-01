a2c54c6544019f070e3912057446d3e07c0fada9
Alexander Artemenko <svetlyak.40wt@gmail.com>
2014-11-01 21:23:54 +0000

    Fixed command get_users_info to be able to pipe output into 'head' command or file.

--- allmychanges/management/commands/get_users_info.py
+++ allmychanges/management/commands/get_users_info.py
@@ -1,4 +1,7 @@
 # coding: utf-8
+import codecs
+import sys
+
 from django.core.management.base import BaseCommand
 from django.template.defaultfilters import timesince
 from twiggy_goodies.django import LogMixin
@@ -10,17 +13,24 @@ class Command(LogMixin, BaseCommand):
     help = u"""Drops user and all his packages."""
 
     def handle(self, *args, **options):
+        writer = codecs.getwriter('utf-8')(sys.stdout)
+        def prn(text):
+            writer.write(text)
+            writer.write('\n')
+
         for user in User.objects.order_by('-id'):
-            print user.username
-            print '\tJoined:', timesince(user.date_joined) + ' ago'
-            print '\tEmail:', user.email
+            prn(user.username)
+            prn(u'\tJoined: {0} ago'.format(timesince(user.date_joined)))
+            prn(u'\tEmail: {0}'.format(user.email))
             packages = list(user.changelogs.all())
 
             if packages:
-                print '\tPackages:'
+                prn('\tPackages:')
                 for changelog in user.changelogs.all():
-                    print '\t\t', changelog.namespace, changelog.name, '(http://allmychanges.com' + changelog.get_absolute_url() + ')'
+                    prn(u'\t\t{0} {1} (http://allmychanges.com{2})'.format(changelog.namespace,
+                                             changelog.name,
+                                             changelog.get_absolute_url()))
             else:
-                print '\tPackages: NO'
+                prn('\tPackages: NO')
 
-            print ''
+            prn('')
