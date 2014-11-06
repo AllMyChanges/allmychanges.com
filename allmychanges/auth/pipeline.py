88afbf30a72b809d758e6d2466cc0d2087c7af61
Alexander Artemenko <svetlyak.40wt@gmail.com>
2014-11-06 00:02:38 +0000

    Fixed issue on new account creation.

--- allmychanges/auth/pipeline.py
+++ allmychanges/auth/pipeline.py
@@ -4,5 +4,5 @@ def add_default_package(strategy, is_new=None, user=None, *args, **kwargs):
     if is_new:
         changelog, created = Changelog.objects.get_or_create(
             namespace='web', name='allmychanges',
-            source='http+http://allmychanges.com/CHANGELOG.md')
+            source='http://allmychanges.com/CHANGELOG.md')
         user.track(changelog)
