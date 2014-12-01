7a86d9f1c3df46711d87db82876c58f59e5c1863
Alexander Artemenko <svetlyak.40wt@gmail.com>
2014-12-01 12:39:45 +0000

    Fixed after release.

--- allmychanges/management/commands/after_release.py
+++ allmychanges/management/commands/after_release.py
@@ -1,3 +1,5 @@
+import os.path
+
 from django.core.management.base import BaseCommand
 from twiggy_goodies.django import LogMixin
 from allmychanges.utils import graphite_send, slack_send
@@ -9,16 +11,21 @@ class Command(LogMixin, BaseCommand):
 
     def handle(self, *args, **options):
         graphite_send(release=1)
-        with open('.hash-before-release') as f:
-            hash_before_release = f.read().strip()
-            current_hash = git['rev-parse', '--short', 'HEAD']().strip()
-            changes = git['log', '--format=%h %s', '{0}..{1}'.format(hash_before_release,
-                                                                  current_hash)]().strip()
-            changes = changes.split(u'\n')
-            changes = [u'\t* {1} <https://github.com/svetlyak40wt/allmychanges/commit/{0}|{0}>'.format(
-                *item.split(' ', 1))
-                       for item in changes]
-            changes = u'\n'.join(changes)
-            slack_send('Deployed {current_hash}:\n{changes}'.format(
-                current_hash=current_hash,
-                changes=changes))
+        current_hash = git['rev-parse', '--short', 'HEAD']().strip()
+
+        if os.path.exists('.hash-before-release'):
+            with open('.hash-before-release') as f:
+                hash_before_release = f.read().strip()
+                changes = git['log', '--format=%h %s', '{0}..{1}'.format(hash_before_release,
+                                                                      current_hash)]().strip()
+                changes = changes.split(u'\n')
+                changes = [u'\t* {1} <https://github.com/svetlyak40wt/allmychanges/commit/{0}|{0}>'.format(
+                    *item.split(' ', 1))
+                           for item in changes]
+                changes = u'\n'.join(changes)
+                slack_send('Deployed {current_hash}:\n{changes}'.format(
+                    current_hash=current_hash,
+                    changes=changes))
+        else:
+            slack_send('Deployed {current_hash}'.format(
+                    current_hash=current_hash))
