60e768b3ce3f59e29665240d772a085c370cfa63
Alexander Artemenko <svetlyak.40wt@gmail.com>
2015-05-21 12:15:04 +0000

    Fix typo in follow-login popup.

--- allmychanges/static/allmychanges/js/components/track-button.js
+++ allmychanges/static/allmychanges/js/components/track-button.js
@@ -52,7 +52,7 @@ module.exports = React.createClass({
             popup = (
                 <div className="modal-popup" onClick={this.handle_popup_click}>
                     <div className="modal-popup__content modal-popup__please-login">
-                      <p>Good job! You\'ve made first step, tracking this package.</p>
+                      <p>Good job! You have made the first step, tracking this package.</p>
                       <p>Now, to receive notifications about future updates, you need to login via:</p>
                       <p><a className="button _good _large" href="/login/twitter/"><i className="fa fa-twitter fa-lg"></i> Twitter</a> or <a className="button _good _large" href="/login/github/"><i className="fa fa-github fa-lg"></i> GitHub</a></p>
                     </div>
