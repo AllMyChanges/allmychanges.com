40f1d0d316ee42428f35a60a28bc5cacde7e8abe
Alexander Artemenko <svetlyak.40wt@gmail.com>
2015-06-12 23:34:09 +0000

    Поправлен create env.

--- scripts/create-env.sh
+++ scripts/create-env.sh
@@ -4,7 +4,7 @@ ENVDIR=env
 
 if [ ! -e $ENVDIR/bin/python ]; then
 #    env3/bin/virtualenv --python=/usr/bin/python --system-site-packages --distribute $ENVDIR
-    env3/bin/virtualenv --python=/usr/bin/python $ENVDIR
+    virtualenv --python=/usr/bin/python $ENVDIR
     $ENVDIR/bin/pip install -U pip
     $ENVDIR/bin/pip install -U -r requirements.txt
     $ENVDIR/bin/pip install -e `pwd`
