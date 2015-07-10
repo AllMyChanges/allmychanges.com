45d8d856d828281dd0a805dad1427add001f2572
Alexander Artemenko <svetlyak.40wt@gmail.com>
2015-07-10 13:32:19 +0000

    Revert "Slightly changed create-env."
    This reverts commit 8ca925e52bd51fc195eea494351128f7101917f9.

--- scripts/create-env.sh
+++ scripts/create-env.sh
@@ -4,8 +4,9 @@ ENVDIR=env
 
 if [ ! -e $ENVDIR/bin/python ]; then
 #    env3/bin/virtualenv --python=/usr/bin/python --system-site-packages --distribute $ENVDIR
-    /usr/bin/virtualenv --python=/usr/bin/python $ENVDIR
+    virtualenv --python=/usr/bin/python $ENVDIR
     $ENVDIR/bin/pip install -U pip
     $ENVDIR/bin/pip install -U -r requirements.txt
     $ENVDIR/bin/pip install -e `pwd`
-fi
+
+    fi
