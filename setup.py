ad6f9819f19ea764d854afdbc2e755e7da7061a5
Alexander Artemenko <svetlyak.40wt@gmail.com>
2016-08-01 23:03:44 +0300

    Bump version: 1.7.0 → 1.7.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='allmychanges-website',
-    version='1.7.0',
+    version='1.7.1',
     description='A djangodash project.',
     author='Alexander Artemenko',
     author_email='svetlyak.40wt@gmail.com',
