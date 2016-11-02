16a22ebfd8f5e1d75286bafb1667c67ea4c98c06
Alexander Artemenko <svetlyak.40wt@gmail.com>
2016-11-02 08:58:52 +0300

    Added ability to debug emails by saving them into the 'emails' folder.

--- allmychanges/notifications/email.py
+++ allmychanges/notifications/email.py
@@ -6,9 +6,18 @@ from django.conf import settings
 from premailer import Premailer
 
 
-def send_email(recipient, subject, template, context={}, tags=[]):
+def send_email(recipient, subject, template, context={}, tags=[], debug=False):
     body = _render_body(template, context)
-    _send_email(body, subject, recipient, tags)
+    if debug:
+        filename = '/app/emails/{0}.html'.format(
+            recipient.replace('@', '-at-'))
+
+        with open(filename, 'w') as f:
+            f.write(body.encode('utf-8'))
+            print 'Email for', recipient, 'was written to', filename
+
+    else:
+        _send_email(body, subject, recipient, tags)
 
 
 def _send_email(body, subject, recipient, tags):
